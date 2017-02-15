4b76b02f57b31744b50eb99cea873766ceb5626a
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-02-15 23:21:58 +0100

    I think it is working SGD

---
+++ count_step_sgd_data.m
@@ -0,0 +1,1 @@
+function [B, grad, H] = count_step_data(X, Y, W, l, n)  B = diag(1./(1+e.^(Y.*(X*W))));  H = X'*B*(eye(n)-B)*X;  if(det(H) == 0)    H = l*eye(m) + X'*B*(eye(n)-B)*X;    grad = l*W - X'*B*Y;  else    grad = (-1)*X'*B*Y;  endend
